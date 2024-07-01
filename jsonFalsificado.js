4e35c6dc7f7311ea69b759bafaf7f1e24858f9c0
Edgar Amaya <160590740+edgaramayaa10@users.noreply.github.com>
2024-07-01 11:51:10 +0200

    first commit js

---
+++ jsonFalsificado.js
@@ -0,0 +1,7 @@
+const datos = [
+{ id :1, titular: 'España vs Georgia', contenido: 'España gana 4-1 a Georgia'},
+{ id: 2,  titular: 'Suiza vs Italia', contenido: 'Suiza gana 2-1 a Italia'},
+{ id: 3,  titular: 'Alemania vs Dinamarca', contenido: 'Alemania gana 2-0 a Dinamarca'},
+{ id: 4, titular: 'Inglaterra vs Eslovaquia', contenido: 'Inglaterra gana 2-1 a Eslovaquia'},
+{ id: 5,  titular:'Argentina vs Peru', contenido: 'Argentina gana 2-0 a Peru'},
+];
